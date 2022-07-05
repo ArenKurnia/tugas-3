24d0ddf7d3e03c77357df845e336db7144ad1202
mzhans <98946477+mzhans@users.noreply.github.com>
2022-07-05 13:58:20 +0700

    Commit tugas-3

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import ReactDOM from "react-dom/client";
+
+function Welcome(props){
+    return <h1>Selamat Datang {props.nama}</h1>
+}
+const pengguna = "Aren Kurnia";
+
+function App(){
+    if(pengguna){
+        return <Welcome nama={pengguna}/>
+    }
+    else{
+        return <button>Login</button>
+    }
+}
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
